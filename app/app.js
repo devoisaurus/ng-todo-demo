50a86e74cd967f7ada76883a98a58b509420c648
Andi Devois <devoisaurus@MacBook-Pro.local>
2016-05-21 10:12:54 -0500

    buttons and clicks

--- app/app.js
+++ app/app.js
@@ -6,5 +6,14 @@ app.controller("NavCtrl", function($scope){
 
 app.controller("TodoCtrl", function($scope){
 	$scope.welcome = "hello";
+	$scope.showListView = true;
 
+	$scope.newItem = function(){
+		console.log("you clicked new item");
+		$scope.showListView = false;
+	};
+	$scope.allItem = function(){
+		console.log("you clicked all items");
+		$scope.showListView = true;
+	};
 });
