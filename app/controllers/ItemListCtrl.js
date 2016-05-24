6b060e48a760b28bac1df29857327ce9f7da9b2d
Andi Devois <devoisaurus@MacBook-Pro.local>
2016-05-23 19:19:03 -0500

    firebase get and post

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,35 +1,13 @@
-app.controller("ItemListCtrl", function($scope){
-	$scope.items = [
-	{
-		id: 0,
-		task: "mow the lawn",
-		isCompleted: true,
-		dueDate: "12/5/17",
-		assignedTo: "greg",
-		location: "Zoe's house",
-		urgency: "low",
-		dependencies: "sunshine, clippers, hat, water, headphones"
-			},
-			{
-		id: 1,
-		task: "grade quizzes",
-		isCompleted: false,
-		dueDate: "12/5/15",
-		assignedTo: "Joe",
-		location: "NSS",
-		urgency: "high",
-		dependencies: "wifi, tissues, vodka"
-			},
-			{
-		id: 2,
-		task: "take a nap",
-		isCompleted: false,
-		dueDate: "5/21/16",
-		assignedTo: "zoe",
-		location: "Zoe's house",
-		urgency: "medium",
-		dependencies: "hammock, cat, pillow, blanket"
-			}
-		];
+app.controller("ItemListCtrl", function($scope, $http){
+	$scope.items = [];
+
+	$http.get("https://ad-to-do-app.firebaseio.com/items.json")
+	.success(function(itemObject){
+		var itemCollection = itemObject;
+		Object.keys(itemCollection).forEach(function(key){
+			itemCollection[key].id=key;
+			$scope.items.push(itemCollection[key]);
+		})
+	});
 
 });
--- app/controllers/ItemNewCtrl.js
+++ app/controllers/ItemNewCtrl.js
@@ -1,44 +1,31 @@
-app.controller("ItemNewCtrl", function($scope){
-	$scope.newTask = {};
-
-	$scope.items = [
-	{
-		id: 0,
-		task: "mow the lawn",
-		isCompleted: true,
-		dueDate: "12/5/17",
-		assignedTo: "greg",
-		location: "Zoe's house",
-		urgency: "low",
-		dependencies: "sunshine, clippers, hat, water, headphones"
-			},
-			{
-		id: 1,
-		task: "grade quizzes",
+app.controller("ItemNewCtrl", function($scope, $http, $location){
+	$scope.newTask = {
+		assignedTo: "",
+		dependencies: "",
+		dueDate: "",
 		isCompleted: false,
-		dueDate: "12/5/15",
-		assignedTo: "Joe",
-		location: "NSS",
-		urgency: "high",
-		dependencies: "wifi, tissues, vodka"
-			},
-			{
-		id: 2,
-		task: "take a nap",
-		isCompleted: false,
-		dueDate: "5/21/16",
-		assignedTo: "zoe",
-		location: "Zoe's house",
-		urgency: "medium",
-		dependencies: "hammock, cat, pillow, blanket"
-			}
-		];
+		location: "",
+		task: "",
+		urgency: ""
+	};
 
 	$scope.addNewItem = function(){
-		$scope.newTask.isCompleted = false;
-		$scope.newTask.id = $scope.items.length;
-		console.log("you added a new item", $scope.newTask);
-		$scope.items.push($scope.newTask);
-		$scope.newTask = "";
+		$http.post(
+			"https://ad-to-do-app.firebaseio.com/items.json",
+			JSON.stringify({
+				assignedTo: $scope.newTask.assignedTo,
+				dependencies: $scope.newTask.dependencies,
+				dueDate: $scope.newTask.dueDate,
+				isCompleted: $scope.newTask.isCompleted,
+				location: $scope.newTask.location,
+				task: $scope.newTask.task,
+				urgency: $scope.newTask.urgency
+			})
+		).success(function(response){
+			console.log(response);
+			$location.url("/items/list")
+		})
+
+
 	};
 });
