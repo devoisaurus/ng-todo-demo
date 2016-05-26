bed7c308ca8be3e987ccd49e8c36956912576f0c
Andi Devois <devoisaurus@MacBook-Pro.local>
2016-05-25 21:03:03 -0500

    edit button working

---
+++ app/controllers/ItemEditCtrl.js
@@ -0,0 +1,16 @@
+app.controller("ItemEditCtrl", function($scope, $http, $location, itemStorage, $routeParams){
+    $scope.newTask = {};
+
+    itemStorage.getSingleItem($routeParams.itemId)
+    .then(function successCallback(response){
+        $scope.newTask=response;
+    });
+
+    $scope.addNewItem = function(){
+        itemStorage.updateItem($routeParams.itemId, $scope.newTask)
+            .then(function successCallback(response) {
+                console.log(response);
+                $location.url("/items/list");
+            });
+    };
+});
